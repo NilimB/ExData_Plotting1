62b6375a64af22023044f16b070514d3c9fd5f12
NilimB <borah.nilim001@gmail.com>
2018-08-04 23:02:42 +0530

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,14 @@
+rm(list = ls())
+# Data preprocessing
+setwd("C:/Users/borah/Desktop/Data Science/John Hopkins University/Exploratory Data Analysis/week1/Project")
+epc <- "household_power_consumption.txt"
+dataEpc <- read.table(epc, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+dataEpc$Date <- as.Date(dataEpc$Date, format = "%d/%m/%Y")
+dataEpc <- subset(dataEpc, subset = (Date >= "2007-02-01" & Date <= "2007-02-02"))
+
+
+# Plot 1
+globalActivePower <- as.numeric(dataEpc$Global_active_power)
+png("plot1.png", width=480, height=480)
+hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
