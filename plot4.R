15d43a7ea131e955bb462a19897df76b6dcdc745
NilimB <borah.nilim001@gmail.com>
2018-08-05 00:14:43 +0530

    Create plot4.R

---
+++ plot4.R
@@ -0,0 +1,22 @@
+rm(list = ls())
+# Data preprocessing
+dataEpc <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?")
+dataEpc$Date <- as.Date(dataEpc$Date, format = "%d/%m/%Y")
+dataEpc <- subset(dataEpc, subset = (Date >= "2007-02-01" & Date <= "2007-02-02"))
+dataEpc$datetime <- strptime(paste(dataEpc$Date, dataEpc$Time), "%Y-%m-%d %H:%M:%S")
+
+# Plot 4
+dataEpc$datetime <- as.POSIXct(dataEpc$datetime)
+par(mfrow = c(2, 2))
+png("plot4.png", width=480, height=480)
+plot(dataEpc$datetime, dataEpc$Global_active_power, type = "l", ylab = "Global Active Power", xlab = "")
+plot(dataEpc$datetime, dataEpc$Voltage, type = "l")
+plot(dataEpc$datetime, dataEpc$Sub_metering_1, type = "l", ylab = "Energy sub metering", xlab = "")
+lines(dataEpc$datetime, dataEpc$Sub_metering_2,  col = "Red")
+lines(dataEpc$datetime, dataEpc$Sub_metering_3,  col = "Blue")
+legend("topright", lty = 1, col = c("black", "red", "blue"), 
+legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),bty = "n")
+plot(dataEpc$datetime, dataEpc$Global_active_power, type = "l")
+dev.off()
+
+
